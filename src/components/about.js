484530ab77c289731c3e702582fb1f3c9d7ff4ce
Sam Oberly <soberly1@jh.edu>
2022-10-31 23:48:36 +0000

    fixed about

--- src/components/about.js
+++ src/components/about.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function About() {
   return (
-    <div>
+    <div className="about">
       <h2>About</h2>
       <p>
         I am a student at Johns Hopkins University and University of Cambridge
