6f57147bc9e1b8dbde3ef1c0fe67c95bb7548f63
Sam Oberly <soberly1@jh.edu>
2022-11-01 00:21:08 +0000

    Done!

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,18 @@
 import React from "react";
-
+import fb from "../images/Facebook Icon.png";
+import github from "../images/GitHub Icon.png";
+import insta from "../images/Instagram Icon.png";
+import twitter from "../images/Twitter Icon.png";
 export default function Footer() {
-  return <h4>This is the Footer</h4>;
+  return (
+    <div className="footer">
+      <img src={twitter}></img>
+
+      <img src={fb}></img>
+
+      <img src={insta}></img>
+
+      <img src={github}></img>
+    </div>
+  );
 }
